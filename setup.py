04ac051aa59a1ac770d79d850ccdff2159ef7b5b
TTCooper-PhD <tcoope2@gmail.com>
2022-12-02 18:54:57 -0500

    Updated

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
+
+VERSION='0.1.0'
+
+
 setup(
     name="Peptide_Chef",
-    version="0.0.11",
+    version=VERSION,
     author="Tyler T. Cooper",
-    author_email="tcoope2@uwo.ca",
+    author_email="tcoope2@gmail.com"
+    description="An all-in-one Python Libary for Proteomic Data Analyses"
     long_description=open('README.txt').read()+ '\n\n' + open('CHANGELOG.txt').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/TTCooper-PhD",
@@ -17,7 +22,7 @@ setup(
         "Operating System :: OS Independent",
         'Intended Audience :: Education'
     ],
-    keywords='Proteomics',
+    keywords=['Proteomics',"Python"."DataAnalysis"],
     python_requires=">=3.6",
     install_requires=["json","numpy","pandas","seaborn","matplotlib","itertools","re","gzip","urllib"]
     packages=find_packages(include=["Peptide_Chef"]),
